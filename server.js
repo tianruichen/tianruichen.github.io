b32807366b2292a59a6674d9cdd39d40e1c0d5d8
tianruichen <tianruichen@users.noreply.github.com>
2015-10-23 21:37:43 -0700

    testing initial commit

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require("express"),
-    app = express(),
-    router = express.Router();
-
-app.use(express.static('public'));
-
-router.get("/", function(req, res){
-    //res.sendFile(__dirname + '/public/inputpage.html');
-});
